0a61127fa07e3b5fc75b2bfbc3994e114a0157cb
oshkuk2277 <oshkuk@yandex.ru>
2020-04-02 16:22:54 +0300

    Fourth task is executed

--- 4.py
+++ 4.py
@@ -1,14 +1,18 @@
+# 4. Пользователь вводит строку из нескольких слов, разделённых пробелами.
+# Вывести каждое слово с новой строки. Строки необходимо пронумеровать.
+# Если в слово длинное, выводить только первые 10 букв в слове.
+
+entered_string = (input('Input the number of list items:'))
+list_string = entered_string.split(" ")
+
+for i in range(len(list_string)):
+
+    if len(list_string[i]) > 10:
+        print(f'{i+1}th word: {list_string[i][:10]}')
+
+    else:
+        print(f'{i + 1}th word: {list_string[i]}')
 
 
 
 
-
-flag = True
-
-while flag:
-    try:
-        items_list = int(input('Input the number of list items:'))
-        flag = False
-
-    except ValueError:
-        print('Entered items_list is not a number!!!')
